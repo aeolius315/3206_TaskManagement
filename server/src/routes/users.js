7ccc844c5590db6a967292994586ec9ea0804c93
“aeolius315” <ralphkintana02@gmail.com>
2024-05-15 21:05:30 +0800

    Cleaned the Code

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -21,7 +21,7 @@ router.post("/register", async (req,res) => {
     res.json({ message: "User Successfully Registered", newUser });
 });
 
-router.get("/login", async (req,res) => {
+router.post("/login", async (req,res) => {
     const { username, password } = req.body;
     const user = await UserModel.findOne({ username });
 
